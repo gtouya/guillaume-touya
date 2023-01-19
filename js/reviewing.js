2ea8a8afabb00368eb2034ea53ef0a6ac59ed0f4
gtouya <guillaume.touya@ign.fr>
2023-01-19 10:59:51 +0100

    update reviews

--- js/reviewing.js
+++ js/reviewing.js
@@ -24,7 +24,7 @@ const CVIS = {
     BOOK_LIST: ["Books"],
     JOUR_LIST: ["IJGIS", "CaGIS", "TiGIS", "JOSIS", "IJGI", "IJC", "Carto", "Other GIS", "SpatialCog", "IEEE", "ISPRS", "EP:B"],
     CONF_LIST: ["ICC", "GIScience", "GeneICA", "SAGEO", "ISPRS", "ISSDQ", "SAC", "EuroCarto", "CompCarto"],
-    TIME_INTERVAL: ["2006", "2023"],
+    TIME_INTERVAL: ["2006", "2024"],
     SVG_W: 800,
     TITLE_Y: 26,
     CAPTION_X: 40,
