5b0a8988869581c17f13dd372658d7c13ddc29f6
gtouya <guillaume.touya@ign.fr>
2025-06-25 12:26:00 +0200

    Update

--- js/reviewing.js
+++ js/reviewing.js
@@ -23,8 +23,8 @@
 const CVIS = {
     BOOK_LIST: ["Books"],
     JOUR_LIST: ["IJGIS", "CaGIS", "TiGIS", "JOSIS", "IJGI", "IJC", "Carto", "Other GIS", "SpatialCog", "IEEE", "ISPRS", "EP:B", "CEUS"],
-    CONF_LIST: ["ICC", "GIScience", "GeneICA", "SAGEO", "ISPRS", "ISSDQ", "SAC", "EuroCarto", "CompCarto", "CartoAI", "ISCRAM"],
-    TIME_INTERVAL: ["2009", "2025"],
+    CONF_LIST: ["ICC", "GIScience", "GeneICA", "SAGEO", "ISPRS", "ISSDQ", "SAC", "EuroCarto", "CompCarto", "CartoAI", "AGILE", "ISCRAM"],
+    TIME_INTERVAL: ["2009", "2026"],
     SVG_W: 800,
     TITLE_Y: 26,
     CAPTION_X: 40,
